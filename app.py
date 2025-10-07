fcb7932809bbd5449d04aa7af3d403e7342f3c05
aledrinyth <58565713+aledrinyth@users.noreply.github.com>
2025-10-08 01:40:30 +1100

    Update app.py
    Removed from webdriver_manager.chrome import ChromeDriverManager since webdriver is downloaded in dockerfile

--- app.py
+++ app.py
@@ -3,7 +3,6 @@ from flask import Flask, request, jsonify
 import pandas as pd
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
-from webdriver_manager.chrome import ChromeDriverManager
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.chrome.options import Options
